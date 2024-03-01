0e8aeb163ec008b47f468917fbd34de4ce8462a3
Shirley Tatiana <stgarciac@ufpso.edu.co>
2024-02-29 21:08:18 -0500

    First commit

---
+++ shopping-store/src/test/java/com/process/shoppingstore/ShoppingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.process.shoppingstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
